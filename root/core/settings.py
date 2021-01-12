746fd2495264c45ddf615efe6627c1cab1b0bc13
mikhpo <66635392+mikhpo@users.noreply.github.com>
2021-01-12 20:49:45 +0300

    initial deploy

--- root/core/settings.py
+++ root/core/settings.py
@@ -104,8 +104,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "favicon"),
 ]
 
 MEDIA_URL = '/media/'
