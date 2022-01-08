ddae3e97e9da02066a16ba88976ba90245caa188
Mikhail Polyakov <mikhpo@outlook.com>
2022-01-08 18:45:55 +0300

    add cron jobs script finished

--- utils/add_cron_jobs.sh
+++ utils/add_cron_jobs.sh
@@ -9,17 +9,13 @@
 # Получение содержимого текущего crontab файла.
 cron=`crontab -l 2> /dev/null`
 
-if [[ -z "$cron" ]]; then
-    echo "У пользователя не создан crontab"
-fi
-
 # Массив задач на добавление.
 cron_jobs=(
     "@reboot (cd /home/mikhpo/personal-website/ && source .venv/bin/activate && python3 root/manage.py run_scheduler)"
 )
 
-# Определение символа перехода на новую строку.
-NEWLINE=$"\n"
+# Определение символа перехода на новую строку. 
+NEWLINE=$'\n'
 
 # Цикл для каждого элемента из массива задач на добавление.
 for job in "${cron_jobs[@]}"
@@ -31,10 +27,20 @@ for job in "${cron_jobs[@]}"
         else
             # Если задача не найдена в crontab, то она добавляется
             # к текущему crontab в новой строке.
-            cron="${cron}${NEWLINE}${job}"
-            echo "Задача \""$job"\" добавлена в crontab"
+            if [[ -z "$cron" ]]; then
+                # Если в данный момент crontab пуст или не создан, 
+                # то задача записывается в первую строку файла.
+                cron="${job}"
+            else
+                # Если файл не пуст, то задача добавляется с новой строки.
+                cron="${cron}${NEWLINE}${job}"
+            fi
+            echo "Задача \""$job"\" добавлена в crontab"           
         fi
     done
+
+# Обязательное добавление пустой строки в конец crontab.
+cron="${cron}${NEWLINE}"
 
 # Сохранение результата в crontab.
 echo "$cron" | crontab -
