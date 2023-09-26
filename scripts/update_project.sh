3af151efa5c8c95e081f3d955cc8e4b1f0e20813
Mikhail Polyakov <mikhpo@outlook.com>
2023-09-26 17:40:15 +0000

    Fix update_project.sh

--- scripts/update_project.sh
+++ scripts/update_project.sh
@@ -8,14 +8,14 @@
 # 4. Перезапуск Gunicorn.
 # 5. Перезапуск Nginx. 
 # Адрес корневого каталога проекта определяется автоматически.
-repository_root="$(dirname "$(dirname "$(readlink -fm "$0")")")"
+project_root="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 project_name="personal_website"
 python="$project_root/.venv/bin/python"
-$script="$repository_root/$project_name/manage.py"
-cd $repository_root
+script="$project_root/$project_name/manage.py"
+cd $project_root
 git fetch origin
 git pull
-bash ./scripts/install_dependencies.sh
-& $python $script migrate 
-bash ./scripts/add_cron_jobs.sh
-bash ./scripts/restart_services.sh
+bash $project_root/scripts/install_dependencies.sh
+$python $script migrate 
+bash $project_root/scripts/add_cron_jobs.sh
+bash $project_root/scripts/restart_services.sh
