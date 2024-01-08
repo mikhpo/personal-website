04d44d404f700a0720aafd66293f97a7c5f7abfd
Mikhail Polyakov <mikhpo@outlook.com>
2024-01-08 23:11:47 +0300

    Add test with compose workflow

--- tools/runtests.sh
+++ tools/runtests.sh
@@ -1,25 +1,49 @@
 #!/bin/bash
 #
-# Запуск тестов проекта (Python и JavaScript) с формированием отчета Coverage.
+# Запуск тестов проекта Python с формированием отчета Coverage и тестов JavaScript.
 
 #######################################
-# Вывести в терминал линию для разделения этапов выполнения скрипта.
+# Вывести в терминал линию для разделения 
+# этапов выполнения скрипта.
 #######################################
 function print_line() {
     printf %"$(tput cols)"s |tr " " "="
 }
 
+#######################################
 # Смена директории на корневую директорию проекта.
-readonly repository_root="$(dirname "$(dirname "$(readlink -f "$0")")")"
-cd $repository_root
+#######################################
+function change_dir() {
+    repository_root="$(dirname "$(dirname "$(readlink -f "$0")")")"
+    cd "$repository_root" || exit
+}
 
+#######################################
 # Запустить тесты Node.js.
-print_line
-echo "Выполнение тестов Node.js"
-npm test
+#######################################
+function run_npm_test() {
+    print_line
+    echo "Выполнение тестов Node.js"
+    npm test
+}
 
-# Запустить тесты Django при помощи Pytest.
-print_line
-echo "Выполнение тестов Django"
-poetry run coverage run -m pytest
-poetry run coverage html
+#######################################
+# Запустить тесты проекта Django при помощи Pytest.
+#######################################
+function run_project_tests() {
+    print_line
+    echo "Выполнение тестов Django"
+    poetry run coverage run -m pytest
+    poetry run coverage html
+}
+
+#######################################
+# Основная бизнес-логика скрипта.
+#######################################
+function main() {
+    change_dir
+    run_npm_test
+    run_project_tests
+}
+
+main "$@"
