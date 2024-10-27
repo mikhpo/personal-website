dce0b4c78cf26bca12263949f5581655677dcf50
Mikhail Polyakov <mikhpo@outlook.com>
2024-10-27 19:18:03 +0300

    Добавлена фабрика для серий в блоге

--- personal_website/blog/factories.py
+++ personal_website/blog/factories.py
@@ -1,7 +1,7 @@
 """Фабрики для генерации экземпляров классов с фейковыми данными для тестирования."""
 import factory
 
-from blog.models import Category, Topic
+from blog.models import Category, Series, Topic
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
@@ -35,4 +35,21 @@ class TopicFactory(factory.django.DjangoModelFactory):
 
     def __new__(cls, *args, **kwargs) -> "Topic":
         """Возвращается объект Topic."""
+        return super().__new__(*args, **kwargs)
+
+
+class SeriesFactory(factory.django.DjangoModelFactory):
+    """Фабрика для генерации случайных данных для модели Series."""
+
+    class Meta:  # noqa: D106
+        model = Series
+
+    name = factory.Faker("word")
+    description = factory.Faker("sentence")
+    slug = factory.LazyAttribute(lambda _: None)
+    image = factory.django.ImageField()
+    public = factory.Faker("boolean")
+
+    def __new__(cls, *args, **kwargs) -> "Series":
+        """Возвращается объект Series."""
         return super().__new__(*args, **kwargs)
--- personal_website/blog/tests/test_factories.py
+++ personal_website/blog/tests/test_factories.py
@@ -1,13 +1,13 @@
 """Тесты фабрик для генерации экземпляров моделей со случайными данными."""
 from django.test import TestCase
 
-from blog.factories import CategoryFactory, TopicFactory
-from blog.models import Category, Topic
+from blog.factories import CategoryFactory, SeriesFactory, TopicFactory
+from blog.models import Category, Series, Topic
 from gallery.utils import is_image
 
 
 class TestCategoryFactory(TestCase):
-    """Тесты фабрики для создания категорий."""
+    """Тесты фабрики для создания категорий статей в блоге."""
 
     def test_category_factory_instance(self) -> None:
         """Фабрика возвращает объект категории."""
@@ -22,7 +22,7 @@ class TestCategoryFactory(TestCase):
 
 
 class TestTopicFactory(TestCase):
-    """Тесты фабрики для создания темы."""
+    """Тесты фабрики для создания темы статьи в блоге."""
 
     def test_topic_factory_instance(self) -> None:
         """Фабрика возвращает объект темы."""
@@ -33,4 +33,19 @@ class TestTopicFactory(TestCase):
         """Поле изображения объекта содержит настоящее изображение."""
         topic: Topic = TopicFactory()
         is_valid_image = is_image(topic.image)
+        self.assertTrue(is_valid_image)
+
+
+class TestSeriesFactory(TestCase):
+    """Тесты фабрики для создания серии статей в блоге."""
+
+    def test_series_factory_instance(self) -> None:
+        """Фабрика возвращает объект серии."""
+        series = SeriesFactory()
+        self.assertIsInstance(series, Series)
+
+    def test_series_factory_image_field(self) -> None:
+        """Поле изображения объекта содержит настоящее изображение."""
+        series: Series = SeriesFactory()
+        is_valid_image = is_image(series.image)
         self.assertTrue(is_valid_image)
